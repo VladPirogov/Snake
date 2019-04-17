54d7317bcdec7532a4e28cb5d9e989d3fd57be94
Vlad Pirogov <pirogov1818@gmail.com>
2019-04-17 13:24:15 +0300

    L5

---
+++ Assets/Scripts/Borders.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Borders : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if(other.CompareTag("SnakeHead"))
+		{
+				Application.LoadLevel(Application.loadedLevel);
+		}
+
+	}
+}
